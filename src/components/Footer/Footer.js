c032216a68e188b24e7c969f902151eda69db386
Rajat Singh <s.rajat55@gmail.com>
2023-04-18 03:18:14 +0530

    customise portfolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__link'
+    >
+      Created By Sriram Venkat
+    </a>
+  </footer>
+)
+
+export default Footer
